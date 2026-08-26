fe317f11ef194808e73dec728b6d863f4d21a8fc
Cyril Rohr <hey@cyrilrohr.com>
2026-08-26 15:23:10 +0200

    Fix full pipeline E2E coverage

--- package/e2e/install-package.sh
+++ package/e2e/install-package.sh
@@ -5,10 +5,12 @@ target="${1:?target is required}"
 image="${2:?image is required}"
 package_path="${3:?package path is required}"
 command="${4:?package command is required}"
+package_file="/package/$(basename "$package_path")"
 
 case "$target" in
-  ubuntu:*|debian:*) install="dpkg -i /package/$(basename "$package_path")" ;;
-  el:*|sles:*) install="rpm -i /package/$(basename "$package_path")" ;;
+  ubuntu:*|debian:*) install="apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y '$package_file'" ;;
+  el:*) install="dnf install -y '$package_file'" ;;
+  sles:*) install="zypper --non-interactive --no-gpg-checks install '$package_file'" ;;
   *) echo "Unsupported target: $target" >&2; exit 2 ;;
 esac
 
